64f2000c34d1f1986618cbd52e5648ef8353d408
a.chaturov <chaturovai@gmail.com>
2025-03-24 22:27:10 +0300

    Unit 12.1.5

--- Module_12/Program.cs
+++ Module_12/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var users = new User[]
+        {
+            new()
+            {
+                Login = "Ivan333",
+                Name = "Иван",
+                IsPremium = false
+            },
+            new()
+            {
+                Login = "Stepan",
+                Name = "Степан",
+                IsPremium = false
+            },
+            new()
+            {
+                Login = "Bibi",
+                Name = "Альбина",
+                IsPremium = true
+            }
+        };
+
+        for (var i = 0; i < users.Length; i++)
+        {
+            if (!users[i].IsPremium)
+                ShowAds();
+            Console.WriteLine($"Привет, {users[i].Name}!");
+            Console.WriteLine();
+        }
+    }
+
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+    }
+
+    static void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        Thread.Sleep(3000);
     }
 }
